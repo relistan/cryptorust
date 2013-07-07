a4f7c5ab2281b5ec11ba72dd6a04353a29fcc649
Karl Matthias <karl@matthias.org>
2013-07-07 17:16:01 +0100

    Support all the other SHA hash methods with their own functions.

--- crypto.rs
+++ crypto.rs
@@ -3,7 +3,13 @@ extern mod std;
 extern mod crypto {
   fn SHA1(src: *u8, sz: libc::c_uint, out: *u8) -> *u8;
   fn MD5(src: *u8, sz: libc::c_uint, out: *u8) -> *u8;
+  fn SHA224(src: *u8, sz: libc::c_uint, out: *u8) -> *u8;
+  fn SHA256(src: *u8, sz: libc::c_uint, out: *u8) -> *u8;
+  fn SHA384(src: *u8, sz: libc::c_uint, out: *u8) -> *u8;
+  fn SHA512(src: *u8, sz: libc::c_uint, out: *u8) -> *u8;
 }
+
+enum HashEngine { MD5, SHA1, SHA224, SHA256, SHA384, SHA512 }
 
 struct Digest { digest: ~[u8] }
 
@@ -37,6 +43,30 @@ fn sha1(data: ~[u8]) -> ~Digest {
   }
 }
 
+fn sha224(data: ~[u8]) -> ~Digest {
+  unsafe {
+    hash(data, crypto::SHA224, 28)
+  }
+}
+
+fn sha256(data: ~[u8]) -> ~Digest {
+  unsafe {
+    hash(data, crypto::SHA256, 32)
+  }
+}
+
+fn sha384(data: ~[u8]) -> ~Digest {
+  unsafe {
+    hash(data, crypto::SHA384, 48)
+  }
+}
+
+fn sha512(data: ~[u8]) -> ~Digest {
+  unsafe {
+    hash(data, crypto::SHA512, 64)
+  }
+}
+
 fn md5(data: ~[u8]) -> ~Digest {
   unsafe {
     hash(data, crypto::MD5, 16)
@@ -47,16 +77,23 @@ fn xor_with(subject: &str, val: u8) -> ~[u8] {
   do subject.to_bytes().map |&c| { val ^ c }
 }
 
-fn hmac_sha1(key: ~str, message: ~str) -> ~Digest {
-  hmac(key, message, 64, sha1)
-}
-
 fn hmac_md5(key: ~str, message: ~str) -> ~Digest {
   hmac(key, message, 64, md5)
 }
 
-fn hmac(key: ~str, message: ~str, block_size: uint, hash_func: &fn(~[u8]) -> ~Digest) -> ~Digest {
+fn hmac_sha1(key: ~str, message: ~str) -> ~Digest {
+  hmac(key, message, 64, sha1)
+}
 
+fn hmac_sha256(key: ~str, message: ~str) -> ~Digest {
+  hmac(key, message, 64, sha256)
+}
+
+fn hmac_sha512(key: ~str, message: ~str) -> ~Digest {
+  hmac(key, message, 128, sha512)
+}
+
+fn hmac(key: ~str, message: ~str, block_size: uint, hash_func: &fn(~[u8]) -> ~Digest) -> ~Digest {
   let computed_key: &str = match key.len() {
     _ if key.len() > block_size => hash_func(key.to_bytes()).hexdigest(), // TODO this should be .digest more likely
     _ if key.len() < block_size => key + str::from_bytes(vec::from_elem(block_size - key.len(), 0)),
@@ -93,12 +130,10 @@ fn test_hmac_sha1() {
   )
 }
 
-
-/*
 #[test]
 fn test_hmac_sha256() {
   assert_eq!(
-    hmac(SHA256, ~"my key", ~"some data to hmac"), 
+    hmac_sha256(~"my key", ~"some data to hmac").hexdigest(),
     ~"17764a8a2e8c23ae3b9e1781c5ebeb5754c7920806021032e9133051e10118be"
   );
 }
@@ -106,12 +141,11 @@ fn test_hmac_sha256() {
 #[test]
 fn test_hmac_sha512() {
   assert_eq!(
-    hmac(SHA512, ~"my key", ~"some data to hmac"), 
+    hmac_sha512(~"my key", ~"some data to hmac").hexdigest(),
     ~"b54973feba2e436c4f0911855c80e7320d72edd37b359d6474d714718c52775f163832b79214e9fa9d400208d21372a465b2cda2a1d5ab488bbc02b0daea9626"
   );
 }
 
-*/
 #[test]
 fn test_hmac_md5() {
   assert_eq!(hmac_md5(~"my key", ~"some data to hmac").hexdigest(), ~"befd13e00ee07549520ade402c664a3a");
