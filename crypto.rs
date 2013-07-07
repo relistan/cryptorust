e59a21dc40ee85b3fd1ee719388b96898c1af338
Karl Matthias <karl@matthias.org>
2013-07-07 15:14:07 +0100

    Rust-native HMAC now working with some refactoring.
    Currently only supports SHA1, so existing tests for other HMACs
    are commented out for the moment..

--- crypto.rs
+++ crypto.rs
@@ -3,147 +3,127 @@ extern mod std;
 enum HashEngine { SHA1, SHA128, SHA224, SHA256, SHA384, SHA512, MD5 }
 
 extern mod crypto {
-	fn SHA1(src: *u8, sz: libc::c_uint, out: *u8) -> *u8;
-	fn MD5(src: *u8, sz: libc::c_uint, out: *u8) -> *u8;
+  fn SHA1(src: *u8, sz: libc::c_uint, out: *u8) -> *u8;
+  fn MD5(src: *u8, sz: libc::c_uint, out: *u8) -> *u8;
 }
 
 extern mod cryptobindings {
-	fn wrap_HMAC(method: HashEngine, 
-		key: *u8, 
-		key_size: libc::size_t, 
-		data: *u8,
-		size: libc::size_t,
-		target: *u8) -> libc::c_uint;
+  fn wrap_HMAC(method: HashEngine, 
+    key: *u8, 
+    key_size: libc::size_t, 
+    data: *u8,
+    size: libc::size_t,
+    target: *u8) -> libc::c_uint;
 }
 
-fn as_hex(data: ~[u8]) -> ~str {
-	let mut acc = ~"";
-	for data.each |&byte| { acc += fmt!("%02x", byte as uint); }
-	acc
+struct Digest { digest: ~[u8] }
+
+impl Digest {
+    fn new(digest: ~[u8]) -> ~Digest { return ~Digest{ digest: digest } }
+
+    fn hexdigest(&self) -> ~str {
+        let mut acc = ~"";
+        for self.digest.each |&byte| { acc += fmt!("%02x", byte as uint); }
+        acc
+    }
 }
 
-fn hash(data: ~str, hash_func: &fn(*u8, libc::c_uint, *u8) -> *u8, len: uint) -> ~str {
-	unsafe {
-		let bytes = str::to_bytes(data);
-		let hash = hash_func(
-			vec::raw::to_ptr(bytes),
-			bytes.len() as libc::c_uint, ptr::null()
-		);
-		as_hex(vec::from_buf(hash, len))
-	}
+fn hash(data: ~[u8], hash_func: &fn(*u8, libc::c_uint, *u8) -> *u8, len: uint) -> ~Digest {
+  unsafe {
+    Digest::new(
+      vec::from_buf(
+        hash_func(
+          vec::raw::to_ptr(data),
+          data.len() as libc::c_uint, ptr::null()
+        ),
+        len
+      )
+    )
+  }
 }
 
-fn sha1(data: ~str) -> ~str {
-	unsafe {
-		hash(data, crypto::SHA1, 20)
-	}
+fn sha1(data: ~[u8]) -> ~Digest {
+  unsafe {
+    hash(data, crypto::SHA1, 20)
+  }
 }
 
-fn md5(data: ~str) -> ~str {
-	unsafe {
-		hash(data, crypto::MD5, 16)
-	}
+fn md5(data: ~[u8]) -> ~Digest {
+  unsafe {
+    hash(data, crypto::MD5, 16)
+  }
 }
 
-fn hmac(engine: HashEngine, key: ~str, data: ~str) -> ~str {
-	unsafe {
-		let mut digest = vec::from_elem(129, 0);
-		let key_bytes  = str::to_bytes(key);
-		let data_bytes = str::to_bytes(data);
-
-		let result_size = cryptobindings::wrap_HMAC(engine, 
-			vec::raw::to_ptr(key_bytes),
-			key_bytes.len() as libc::size_t,
-			vec::raw::to_ptr(data_bytes),
-			data_bytes.len() as libc::size_t,
-			vec::raw::to_ptr(digest)
-		);
-
-		let result = str::from_bytes(digest);
-		result.slice(0, result_size as uint).to_owned()
-	}
+fn xor_with(subject: &str, val: u8) -> ~[u8] {
+  do subject.to_bytes().map |&c| { val ^ c }
 }
 
-fn xor_with(message: &str, val: u8) -> ~str {
-	str::from_bytes(do message.to_bytes().map |&c| { val ^ c })
-}
+fn hmac(key: ~str, message: ~str) -> ~str {
+  let block_size = 64;
 
-fn hmac_native(key: ~str, message: ~str) -> ~str {
-	let block_size = 64;
+  let computed_key: &str = if key.len() > block_size {
+    sha1(key.to_bytes()).hexdigest() // TODO this should be .digest more likely
+  } else if key.len() < block_size {
+    key + str::from_bytes(vec::from_elem(block_size - key.len(), 0))
+  } else {
+    key
+  };
 
-	let computed_key: &str = if key.len() > block_size {
-		sha1(key)
-	} else if key.len() < block_size {
-		key + str::from_bytes(vec::from_elem(block_size - key.len(), 0))
-	} else {
-		key
-	};
+  let o_key_pad = xor_with(computed_key, 0x5c);
+  let i_key_pad = xor_with(computed_key, 0x36);
+  let bin_message = message.to_bytes();
 
-	let o_key_pad = xor_with(computed_key, 0x5c);
-	println(o_key_pad);
-	let i_key_pad = xor_with(computed_key, 0x36);
-	println(i_key_pad);
-
-	sha1(o_key_pad + sha1(i_key_pad + message))
-}
-
-#[test]
-fn test_hmac_native() {
-	assert_eq!(
-		hmac_native(~"my key", ~"some data to hmac"), 
-		~"331c9bf4d7dc8ad7e9bab2f566c8612042a9f4e2"
-	)
+  sha1(o_key_pad + sha1(i_key_pad + bin_message).digest).hexdigest()
 }
 
 fn main() {
-	if core::os::args().len() < 2 { fail!(~"Nothing to hash: supply an argument") }
-	io::println(sha1(core::os::args()[1].clone()));
+  if core::os::args().len() < 2 { fail!(~"Nothing to hash: supply an argument") }
+  io::println(sha1(core::os::args()[1].clone().to_bytes()).hexdigest());
 }
 
 #[test]
 fn test_sha1() {
-	assert_eq!(sha1(~"testing SHA1"), ~"a07bb2f7f56b7b47e54fd9e29b4629c8967dde4c");
+  assert_eq!(sha1("testing SHA1".to_bytes()).hexdigest(), ~"a07bb2f7f56b7b47e54fd9e29b4629c8967dde4c");
 }
 
 #[test]
 fn test_md5() {
-	assert_eq!(md5(~"testing MD5"), ~"da35df95d34f166890fa759f5e00e94c");
+  assert_eq!(md5("testing MD5".to_bytes()).hexdigest(), ~"da35df95d34f166890fa759f5e00e94c");
 }
 
 #[test]
+fn test_hmac() {
+  assert_eq!(
+    hmac(~"my key", ~"some data to hmac"), 
+    ~"331c9bf4d7dc8ad7e9bab2f566c8612042a9f4e2"
+  )
+}
+
+
+/*
+#[test]
 fn test_hmac_sha1() {
-	assert_eq!(hmac(SHA1, ~"my key", ~"some data to hmac"), ~"331c9bf4d7dc8ad7e9bab2f566c8612042a9f4e2");
+  assert_eq!(hmac(SHA1, ~"my key", ~"some data to hmac"), ~"331c9bf4d7dc8ad7e9bab2f566c8612042a9f4e2");
 }
 
 #[test]
 fn test_hmac_sha256() {
-	assert_eq!(
-		hmac(SHA256, ~"my key", ~"some data to hmac"), 
-		~"17764a8a2e8c23ae3b9e1781c5ebeb5754c7920806021032e9133051e10118be"
-	);
+  assert_eq!(
+    hmac(SHA256, ~"my key", ~"some data to hmac"), 
+    ~"17764a8a2e8c23ae3b9e1781c5ebeb5754c7920806021032e9133051e10118be"
+  );
 }
 
 #[test]
 fn test_hmac_sha512() {
-	assert_eq!(
-		hmac(SHA512, ~"my key", ~"some data to hmac"), 
-		~"b54973feba2e436c4f0911855c80e7320d72edd37b359d6474d714718c52775f163832b79214e9fa9d400208d21372a465b2cda2a1d5ab488bbc02b0daea9626"
-	);
+  assert_eq!(
+    hmac(SHA512, ~"my key", ~"some data to hmac"), 
+    ~"b54973feba2e436c4f0911855c80e7320d72edd37b359d6474d714718c52775f163832b79214e9fa9d400208d21372a465b2cda2a1d5ab488bbc02b0daea9626"
+  );
 }
 
 #[test]
 fn test_hmac_md5() {
-	assert_eq!(hmac(MD5, ~"my key", ~"some data to hmac"), ~"befd13e00ee07549520ade402c664a3a");
+  assert_eq!(hmac(MD5, ~"my key", ~"some data to hmac"), ~"befd13e00ee07549520ade402c664a3a");
 }
-
-#[test]
-fn test_wrap_HMAC() {
-	unsafe {
-		let mut digest = str::to_bytes(~"0123456789012345678901234567890123456789");
-		let key_bytes  = str::to_bytes(~"test key");
-		let data_bytes = str::to_bytes(~"testing HMAC");
-
-		cryptobindings::wrap_HMAC(SHA1, vec::raw::to_ptr(key_bytes), 8 as libc::size_t, vec::raw::to_ptr(data_bytes), 12 as libc::size_t, vec::raw::to_ptr(digest));
-		assert_eq!(str::from_bytes(digest), ~"9d20c8c35c7ce6b1c4283731bf1fe877d24aa8e7");
-	}
-}
+*/
